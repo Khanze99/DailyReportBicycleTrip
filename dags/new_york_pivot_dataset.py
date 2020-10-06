4aa90da5e254a65ee0e6e97904666905c9d7b936
Khanze99 <khamidov_anvar@mail.ru>
2020-10-06 05:12:01 +0300

    #12 add check file and download in pipeline

--- dags/new_york_pivot_dataset.py
+++ dags/new_york_pivot_dataset.py
@@ -1,8 +1,24 @@
+import datetime as dt
 import os
+import logging
+import psycopg2
 
+from google.cloud import storage
 import pandas as pd
 from pandahouse import to_clickhouse
 from dotenv import load_dotenv
+from airflow.operators.python_operator import PythonOperator
+from airflow.models import DAG
+
+
+args = {
+    'owner': 'airflow',
+    'start_date': dt.datetime(2020, 10, 2),
+    'retries': 1,
+    'retry_delay': dt.timedelta(minutes=1),
+    'depends_on_past': False,
+
+}
 
 load_dotenv()
 
@@ -21,6 +37,49 @@ CREATE_TABLES_DICT = {'number_trips': '''CREATE TABLE IF NOT EXISTS new_york.num
                                       avg_duration Float64) ENGINE=ReplacingMergeTree(date, (date), 8192)''',
                       'gender_number_trips': '''CREATE TABLE IF NOT EXISTS new_york.gender_number_trips (date Date,
                                          gender UInt8, count UInt64) ENGINE=ReplacingMergeTree(date, (date), 8192)'''}
+
+client_google_storage = storage.Client()
+bucket = client_google_storage.get_bucket('bucket_bicycle')
+
+logging.basicConfig(filename='../app.log', level=logging.INFO, format='%(asctime)s %(message)s')
+
+
+def remove_files():
+    '''
+    Удаление файлов
+    "Если пригодится"
+    :return:
+    '''
+    list_files = os.listdir(DATA_DIR)
+    for file in list_files:
+        os.remove(DATA_DIR + file)
+
+
+def check_and_download_files():
+    '''
+    Получение списка всех файлов
+    :return:
+    '''
+    for blob in bucket.list_blobs():  # Цикл по проверке новых файлов
+        filename = blob.name
+        flag_file = False
+        with psycopg2.connect(dbname='bservice', user='khanze') as client_psql:
+            cursor = client_psql.cursor()
+            # Пометить файл в базу данных
+            try:
+                cursor.execute('''INSERT INTO file_bucket (name) VALUES ('{filename}');'''.format(filename=filename))
+                logging.info('{filename} file is written to the database'.format(filename=filename))
+                flag_file = True
+            except psycopg2.Error as e:
+                error = e.pgerror
+                logging.error('PSQL: {}'.format(error))
+
+        if flag_file is False:
+            continue
+
+        with open('data/{}'.format(filename), mode='wb') as csv_file:
+            csv_file.write(blob.download_as_string())
+
 
 
 def pivot_dataset_count_numbers():
@@ -89,3 +148,31 @@ def pivot_dataset_trip_gender():
         df_gender = df.groupby(['gender', 'date'])['tripduration'].agg(count='count')
 
         to_clickhouse(df_gender, table='gender_number_trips', connection=connection)
+
+
+with DAG(dag_id='new_york_dataset_pivot', default_args=args, schedule_interval=None) as dag:
+    check_files = PythonOperator(
+        task_id='check_and_download_files',
+        python_callable=check_and_download_files,
+        dag=dag
+    )
+    pivot_number_trips = PythonOperator(
+        task_id='pivot_dataset_count_numbers',
+        python_callable=pivot_dataset_count_numbers,
+        dag=dag
+    )
+
+    pivot_average_duration = PythonOperator(
+        task_id='pivot_dataset_average_trip_duration',
+        python_callable=pivot_dataset_average_trip_duration,
+        dag=dag
+    )
+
+    pivot_gender_trip = PythonOperator(
+        task_id='pivot_dataset_trip_gender',
+        python_callable=pivot_dataset_trip_gender,
+        dag=dag
+    )
+
+    check_files >> [pivot_gender_trip, pivot_average_duration, pivot_number_trips]
+
